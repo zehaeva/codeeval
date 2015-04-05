472ec383d9d70fbe5ece075e1003db3ca8e300cc
zehaeva <zehaeva@gmail.com>
2015-04-05 01:26:49 -0400

    Reverse Words\na more condensed version in Haskell

--- ReverseWords.hs
+++ ReverseWords.hs
@@ -1,11 +1,7 @@
 import System.Environment (getArgs)
 
-revWords :: String -> String
-revWords input = (unwords . reverse . words) input
-
 main = do
 	[inpFile] <- getArgs
 	input <- readFile inpFile
-	let thing = map revWords $ lines input
 	-- print your output to stdout
-	mapM_ putStrLn thing
+	mapM_ putStrLn $ map (unwords . reverse . words) $ lines input
