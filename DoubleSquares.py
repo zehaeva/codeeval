3f661633b0e728e8903a32c3ac7f3b61adb25f5b
zehaeva <zehaeva@gmail.com>
2015-04-16 00:20:24 -0400

    Double Squares Slightly better, it runs faster anyway. pass 100% of test cases and gets a 45/65 score.

--- DoubleSquares.py
+++ DoubleSquares.py
@@ -1,18 +1,22 @@
 import sys
 import math
-test_cases = open(sys.argv[1], 'r')
 
+test_cases = open(sys.argv[1], 'r')
 number_of_cases = None
 for test in test_cases:
     num = int(test.strip())
     if number_of_cases is None:
         number_of_cases = num
     else:
-        max_square = int(math.sqrt(num)) + 1
+        max_square = int(math.sqrt(num))
         counter = 0
         for x in range(0, max_square):
-            remainder = math.sqrt(num - x**2)
-            if remainder - int(remainder) == 0:
-                counter += 1
-        print(int(counter/2))
+            my_square = x**2
+            if my_square > num / 2:
+                break
+            else:
+                remainder = math.sqrt(num - my_square)
+                if remainder - int(remainder) == 0:
+                    counter += 1
+        print(int(counter))
 test_cases.close()
