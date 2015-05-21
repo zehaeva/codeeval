2e4e356a8fda07ded4b95f19cb8a902949af4d7d
zehaeva <zehaeva@gmail.com>
2015-05-20 22:50:00 -0400

    FizzBuzz in C Christ this is fast, also, I forgot how painful strings and pointers are in C. ugh.

--- solutions_in_c/fizzbuzz.c
+++ solutions_in_c/fizzbuzz.c
@@ -2,24 +2,12 @@
 int main(int argc, const char * argv[]) {
     FILE *file = fopen(argv[1], "r");
     char line[1024];
-    char a[2];
-    char b[2];
-    char c[3];
-    int i, first_mod, second_mod, max_value; 
+    char* parse;
+    int first_mod, second_mod, max_value, i; 
     while (fgets(line, 1024, file)) {
     //  Do something with the line
-        a[0] = line[0];
-        a[1] = '\0';
-        b[0] = line[2];
-        b[1] = '\0';
-        c[0] = line[4];
-        c[1] = line[5];
-        c[2] = '\0';
-        printf("%s:: %s: %s: %s\n\n", line, a, b, c);
 
-        first_mod = 3;
-        second_mod = 5;
-        max_value = 20;
+        sscanf(line, "%d %d %d", &first_mod, &second_mod, &max_value);
 
         for (i = 1;i <= max_value;i++) {
             if (i % first_mod == 0) {
@@ -31,8 +19,9 @@ int main(int argc, const char * argv[]) {
             if (i % first_mod != 0 && i % second_mod != 0) {
                 printf("%i", i);
             }
-            printf("\n");
+            printf(" ");
 	    }
+        printf("\n");    
     }
     return 0;
 }
