780c7b54773b5919b71583ac8d4b84786043963a
Howard Canaway <zehaeva@users.noreply.github.com>
2015-10-15 20:11:54 -0400

    Create oddnumbers.java

---
+++ java/oddnumbers.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+    public static void main (String[] args) throws IOException {
+        for (int i = 1;i<100;i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
